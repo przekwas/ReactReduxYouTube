17f0acf69d3665428c440a63707ed841db58f3ca
Luke Przekwas <galactica@The-Galactica.local>
2018-04-06 11:53:53 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Learning React Redux
+
+The goal of this project is to learn Redux by building a Youtube video viewer using React and Redux.
+
+## Running the Project
+
+1) Download or Clone https://github.com/przekwas/ReactReduxYouTube.git
+2) npm install
+3) You'll need to get your own YoutTube API key from Google API and plug it into the YT_KEY variable in  /src/index.js
+4) npm start
+
---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
+import SearchBar from './components/searchbar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/videodetail';
+import YT_API_KEY from '../credentials';
+
+//Plug in your YouTube API key here
+const YT_KEY = YT_API_KEY;
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+        this.videoSearch('smash melee');
+    }
+
+    videoSearch(term) {
+
+        YTSearch({ key: YT_KEY, term: term }, (videos) => {
+            this.setState({ videos, selectedVideo: videos[0] });
+        });
+
+    }
+
+    render() {
+
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300); 
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
